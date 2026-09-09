4186f56c01546b562d82a74a56bc999e26169266
Leficullen <muh.alfi@ui.ac.id>
2026-09-09 17:16:14 +0700

    feat: define model for projects

--- main/models.py
+++ main/models.py
@@ -27,3 +27,19 @@ class Experience(models.Model):
      @property
      def is_ongoing(self):
           return self.ended_at is None
+
+
+class Project(models.Model):
+
+     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+     title = models.CharField(max_length=225)
+     description = models.TextField()
+     image = models.CharField(max_length=255, blank=True, null=True)
+     url_link = models.URLField(blank=True, null=True)
+
+     def __str__(self):
+          return self.title
+
+     @property
+     def is_live(self):
+          return self.url_link is None
